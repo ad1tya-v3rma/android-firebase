15154d8125d186d382975d8c4fddba8c1bd13c60
Manish Kumar Vats <mvats77947794@gmail.com>
2021-10-27 04:16:32 +0530

    integrated firebase real-time database

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.myapplication
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myapplication.databinding.ActivityMainBinding
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,8 +14,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         binding.crashButton.setOnClickListener {
-            throw RuntimeException("Test Crash") // Force a crash
+            // Write a message to the database
+            val database = Firebase.database
+            val myRef = database.getReference("message")
+
+            myRef.setValue("Hello, World!")
         }
+
     }
 }
