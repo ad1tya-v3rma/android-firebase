56d936ca4ac61e709b1520ae071a007b9c52ff1b
Manish Kumar Vats <mvats77947794@gmail.com>
2021-10-27 04:30:46 +0530

    refactor realtime database integration to use any another fb from url

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,9 +2,13 @@ package com.example.myapplication
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.Utils.Constants
 import com.example.myapplication.databinding.ActivityMainBinding
+import com.google.firebase.FirebaseOptions
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.ktx.app
+import com.google.firebase.ktx.initialize
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,14 +18,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initializeFirebase()
 
         binding.crashButton.setOnClickListener {
-            // Write a message to the database
-            val database = Firebase.database
-            val myRef = database.getReference("message")
-
-            myRef.setValue("Hello, World!")
+            val database = Firebase.database(Firebase.app("tesit"))
+            val myRef = database.reference
+            myRef.child("message").setValue("Hello, World!")
         }
 
+    }
+
+    private fun initializeFirebase() {
+        val options = FirebaseOptions.Builder()
+            .setProjectId(Constants.FIREBASE_PROJECT_ID)
+            .setApplicationId(Constants.FIREBASE_APP_ID)
+            .setDatabaseUrl(Constants.FIREBASE_DB_URL)
+            .build()
+            Firebase.initialize(this, options, "tesit")
     }
 }
